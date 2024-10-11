2259682ef63ea2b59e22165b46bff704c73b4d84
Suchawadee Yospranee <bizkitapidev@gmail.com>
2024-10-11 09:56:24 +0700

    Changes Item table component

--- src/components/ItemTable.vue
+++ src/components/ItemTable.vue
@@ -1,29 +1,75 @@
 <template>
-  <div class="row invoice-body mb-8">
-    <div class="col-xs-12 table-wrapper">
-      <DataTable :value="items" :tableStyle="tableStyle">
-        <Column v-for="col in columns" :key="col.field" :field="col.field" :header="col.header">
-          <template #body="slotProps" v-if="col.template">
-            <slot :name="col.field" v-bind="slotProps">
-              {{ slotProps.data[col.field] }}
-            </slot>
-          </template>
-        </Column>
-      </DataTable>
-    </div>
-  </div>
+  <DataTable 
+    :value="data" 
+    :rows="rows" 
+    :paginator="showPaginator" 
+    :rowsPerPageOptions="[5, 10, 25]" 
+    scrollable
+    :scrollHeight="scrollHeight" 
+    :tableStyle="tableStyle" 
+    v-model:selection="selectedItems" 
+    :dataKey="dataKey"
+    @row-select="onRowSelect" 
+    @row-unselect="onRowUnselect"
+    >
+    <slot name="header"></slot>
+    <Column v-if="selectable" selectionMode="multiple" headerStyle="width: 3rem"></Column>
+    <template v-for="column in columns" :key="column.field">
+      <Column v-bind="column">
+        <template #body="slotProps" v-if="column.customBody">
+          <slot :name="column.field" :data="slotProps.data"></slot>
+        </template>
+      </Column>
+    </template>
+    <slot name="footer"></slot>
+  </DataTable>
 </template>
 
 <script lang="ts" setup>
-interface TableColumn {
+import { ref, computed } from 'vue';
+
+interface ColumnDef {
   field: string;
   header: string;
-  template?: boolean;
+  customBody?: boolean;
+  [key: string]: any;
 }
 
-defineProps<{
-  items: any[];
-  columns: TableColumn[];
+interface ItemTableProps {
+  data: any[];
+  columns: ColumnDef[];
   tableStyle?: string;
+  selectable?: boolean;
+  rows?: number;
+  scrollHeight?: string;
+  dataKey?: string;
+}
+
+const props = withDefaults(defineProps<ItemTableProps>(), {
+  tableStyle: 'min-width: 50rem',
+  selectable: false,
+  rows: 10,
+  scrollHeight: '400px',
+  dataKey: 'id'
+});
+
+const emit = defineEmits<{
+  (e: 'update:selectedItems', items: any[]): void;
+  (e: 'rowSelect', data: any): void;
+  (e: 'rowUnselect', data: any): void;
 }>();
+
+const selectedItems = ref<any[]>([]);
+
+const showPaginator = computed(() => props.data.length > props.rows);
+
+const onRowSelect = (event: { data: any }) => {
+  emit('rowSelect', event.data);
+  emit('update:selectedItems', selectedItems.value);
+};
+
+const onRowUnselect = (event: { data: any }) => {
+  emit('rowUnselect', event.data);
+  emit('update:selectedItems', selectedItems.value);
+};
 </script>
