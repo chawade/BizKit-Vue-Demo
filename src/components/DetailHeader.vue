0acf8025e28341b6c327f45e24ed6a02adcb0bdd
PatcharaponHart <patchrapon00@gmail.com>
2024-10-16 14:21:41 +0700

    edit detailheader

--- src/components/DetailHeader.vue
+++ src/components/DetailHeader.vue
@@ -1,20 +1,21 @@
 <template>
   <div class="detail-head flex justify-between items-end mb-8">
     <div>
-      <h1 class="uppercase font-semibold text-l text-left" style="bottom: 0;">
+      <h1 class="uppercase font-bold text-2xl text-l text-left" style="bottom: 0;">
         <span>{{ title }}</span>
       </h1>
     </div>
-    <div class="text-right">
+    <div class="text-right font-bold text-2xl">
       <h4>{{ itemNo }}</h4>
-      <span v-if="status" class="status" :style="statusStyle">{{ status.StatusName }}</span>
+      <span v-if="statusTheme" class="min-w-28 max-w-28 text-center" :style="statusStyle">{{ status.statusName }}</span>
     </div>
   </div>
 </template>
 
 <script lang="ts" setup>
-import type { StatusResource } from '@/Model/Status';
 import { computed } from 'vue';
+import type { StatusResource } from '@/Model/Status'; 
+import StatusService from '@/service/statusService';
 
 const props = defineProps<{
   title: string;
@@ -22,13 +23,19 @@ const props = defineProps<{
   status: StatusResource;
 }>();
 
+const statusService = new StatusService();
+
+const statusTheme = computed(() => {
+  const statusId = props.status?.statusId || 0; 
+  return statusService.getStatusTheme(statusId);
+});
+
 const statusStyle = computed(() => {
-  if (!props.status) return {};
   return {
-    backgroundColor: props.status.StatusBgColor,
-    color: props.status.StatusFontColor,
-    border: `1px solid ${props.status.StatusBorderColor}`,
-    fontSize: `${props.status.StatusFontSize}px`,
+    backgroundColor: statusTheme.value.bgColor,
+    color: statusTheme.value.fontColor,
+    border: `1px solid ${statusTheme.value.borderColor}`,
+    fontSize: statusTheme.value.fontSize,
     padding: '0.25rem 0.5rem',
     borderRadius: '4px',
     display: 'inline-block'
