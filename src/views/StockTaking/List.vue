b136d364a29c5744a1341fef3a97a28d9f8d3353
Suchawadee Yospranee <bizkitapidev@gmail.com>
2024-10-16 17:55:10 +0700

    Edit stock taking list page

--- src/views/StockTaking/List.vue
+++ src/views/StockTaking/List.vue
@@ -1,6 +1,10 @@
-// StockTakingList.vue
 <template>
-    <div v-if="error">{{ error }}</div>
+    <div v-if="loading" class="justify-center"
+        style="width: 100%; height: 100%; text-align: center; vertical-align: middle;">
+        <ProgressSpinner style="width: 50px; height: 50px" strokeWidth="8" fill="transparent" animationDuration=".5s"
+            aria-label="Custom ProgressSpinner" />
+    </div>
+    <div v-else-if="error">{{ error }}</div>
     <div v-else>
         <div class="card">
             <div class="col-sm-8 flex justify-between mb-8">
@@ -10,58 +14,47 @@
                 </router-link>
             </div>
 
-            <!-- Action buttons and search -->
-            <div class="row">
-                <div class="col-sm-8 mb-5">
-                    <div v-if="permission.EXPORT" class="grid gap-2" role="group">
-                        <div class="col-span-full lg:col-span-8 flex flex-wrap gap-2">
-                            <Button label="Adjust Stock" severity="success" class="w-full sm:w-auto"
-                                @click="approves" />
-                            <Button label="ExportCSV" severity="info" class="w-full sm:w-auto" @click="exportToCSV" />
-                            <Button label="ExportExcel" severity="info" class="w-full sm:w-auto"
-                                @click="exportToExcel" />
-                        </div>
-
-                        <div class="col-span-full lg:col-span-2 mt-2 lg:mt-0 lg:col-start-9">
-                            <span class="p-input-icon-right w-full">
-                                <InputGroup>
-                                    <InputText v-model="searchString" class="w-full" type="text" size="small"
-                                        placeholder="TakingNo, Warehouse" />
-                                    <Button icon="pi pi-search" severity="info" @click="search" />
-                                </InputGroup>
-                            </span>
-                        </div>
-                    </div>
-                </div>
-            </div>
-
             <div class="table-scrollable table-list">
-                <ItemTable 
-                    :data="sortedItems" 
-                    :columns="columns" 
-                    tableStyle="min-width: 50rem" 
-                    :selectable="true"
-                    v-model:selectedItems="selectedItems" 
-                    @rowSelect="onRowSelect" 
-                    @rowUnselect="onRowUnselect"
-                    :rows="10" 
-                    scrollHeight="400px" 
-                    dataKey="TakingId">
+                <ItemTable :items="sortedItems" :columns="columns" :dataKey="'takingId'" :rows-per-page="pageSize"
+                    :pageIdentifier="'stockTaking'" :rowsPerPageOptions="[5, 10, 25]" :selection="selectedItems"
+                    :loading="fetchLoading" :lazy="true" :totalRecords="totalRecords" @page="onPageChange"
+                    @update:selection="onRowSelect" @sort="onSort" @search="fetchData" :menu="menuaa">
                     <template #header>
-                        <Column header="">
-                            <template #body="{ data }">
-                                <!-- <DropdownMenu :data="data" :permission="permission" @action="handleAction" /> -->
+                        <Menubar :model="filteredMenuItems" class="hidden md:flex">
+                            <template #start>
+                                <Button type="button" icon="pi pi-filter-slash" label="Clear" outlined
+                                    @click="clearSearch" />
                             </template>
-                        </Column>
+                            <template #end>
+                                <IconField>
+                                    <InputGroup>
+                                        <InputText v-model="searchString" placeholder="Search..." />
+                                        <Button icon="pi pi-search" severity="info" @click="search" />
+                                    </InputGroup>
+                                </IconField>
+                            </template>
+                        </Menubar>
                     </template>
-                    <template #TakingNo="{ data }">
-                        <router-link :to="`/StockTaking/Detail/${data.TakingId}`" custom v-slot="{ navigate }">
-                            <Button :label="data.TakingNo" link @click="navigate" class="p-0" />
-                        </router-link>
+
+                    <template #takingNo="slotProps">
+                        <RouterLink :to="`/StockTaking/Detail/${slotProps.data.takingId}`" custom v-slot="{ navigate }">
+                            <Button :label="slotProps.data.takingNo" link @click="navigate" class="p-0" />
+                        </RouterLink>
                     </template>
-                    <template #Status="{ data }">
-                        <!-- <StatusBadge :status="data.Status" @click="sortBy(data.Status.StatusName)" /> -->
+
+                    <template #takingDate="slotProps">
+                        {{ new Date(slotProps.data.takingDate).toLocaleDateString() }}
                     </template>
+
+                    <template #status="slotProps">
+                        <Tag class="min-w-28 max-w-28 text-wrap" :value="slotProps.data.status.statusName" :style="{
+                            backgroundColor: statusTheme(slotProps.data.status.statusId).bgColor,
+                            border: `1px solid ${statusTheme(slotProps.data.status.statusId).borderColor}`,
+                            color: statusTheme(slotProps.data.status.statusId).fontColor,
+                            fontSize: statusTheme(slotProps.data.status.statusId).fontSize
+                        }" />
+                    </template>
+
                 </ItemTable>
             </div>
         </div>
@@ -69,27 +62,35 @@
 </template>
 
 <script lang="ts" setup>
-import { ref, computed, onMounted, watch } from 'vue'
-import { useRoute, useRouter } from 'vue-router';
-import StockTakingService from '@/service/stockTakingService';
+import { ref, computed, onMounted, watch, type Ref, reactive, onUnmounted } from 'vue'
+import stockTakingService from '@/service/stockTakingService';
+import type { stockTakingHeaderList } from '@/Model/stockTaking';
+import { useRouter } from 'vue-router';
+import { useToast } from 'primevue/usetoast';
+import { Subscription } from 'rxjs';
 import ItemTable from '@/components/ItemTable.vue';
-import { firstValueFrom } from 'rxjs';
+import type { DataTablePageEvent } from 'primevue/datatable';
+import type { ColumnDef } from '@/Model/GlobalVariable/DataTable';
+import StatusService from '@/service/statusService';
+import { useDataTableStore } from '@/store/useDataTableStore';
 
-const items = ref([])
-const currentPage = ref<number>(1)
-const totalPages = ref<number>(1)
-const pageSize = ref<number>(0)
-const searchString = ref<string>('')
-const sortKey = ref<string>('TakingNo')
-const sortOrder = ref<string>('DESC')
-const totalRecords = ref<number>(0)
-const selectedItems = ref<number[]>([]);
-const selectedTakingIds = ref<number[]>([]);
-const loading = ref<boolean>(false);
-const error = ref(false);
+let subscription: Subscription;
+const statusService = new StatusService();
+const toast = useToast();
+const isSelectAll = ref(false);
 const router = useRouter();
-const route = useRoute();
-
+const dataTableStore = useDataTableStore();
+const items = ref<stockTakingHeaderList[]>([]);
+const currentPage = ref(1)
+const pageSize = ref(5)
+const searchString = ref('')
+const sortKey = ref('takingNo')
+const sortOrder = ref('DESC')
+const totalRecords = ref(0)
+const selectedItems = ref<any[]>([]);
+const loading = ref(false);
+const fetchLoading = ref(true);
+const error = ref(false);
 const permission = ref({
     APPROVE: true,
     EXPORT: true,
@@ -97,155 +98,185 @@ const permission = ref({
     PRINT: true
 });
 
+const menuaa = ref([
+    {
+        label: 'Options',
+        items: [
+            {
+                label: 'Detail',
+                icon: 'pi pi-refresh'
+            },
+            {
+                label: 'Export',
+                icon: 'pi pi-upload'
+            },
+            {
+                label: 'Cancel',
+                icon: 'pi pi-trash'
+            }
+        ]
+    }
+]);
+
+const onSort = (event: any) => {
+    debugger
+    const sortField: string = event.sortField;
+    const Key: number = event.sortOrder;
+    switch (Key) {
+        case 1:
+            sortOrder.value = 'ASC';
+            break;
+        default:
+            sortOrder.value = 'DESC';
+            break;
+    }
+
+    switch (sortField) {
+        case 'takingNo':
+            sortKey.value = 'takingNo';
+            break;
+        case 'takingDate':
+            sortKey.value = 'takingDate';
+            break;
+        case 'warehouse':
+            sortKey.value = 'warehouse';
+            break;
+        case 'location':
+            sortKey.value = 'location';
+            break;
+        case 'personInCharge':
+            sortKey.value = 'personInCharge';
+            break;
+        case 'Status':
+            sortKey.value = 'Status';
+            break;
+        default:
+            sortKey.value = 'takingDate';
+            break;
+    }
+    fetchData();
+};
+
+
+const columns = ref<ColumnDef[]>([
+    { field: 'takingNo', header: 'Taking No', sortable: true, class: 'text-left; width: fit-content;', headerClass: 'w-full min-w-36 text-center font-bold' },
+    { field: 'takingDate', header: 'Taking Date', sortable: true, class: 'width: 15%', headerClass: 'w-full min-w-36 text-center font-bold' },
+    { field: 'warehouseName', header: 'Warehouse', sortable: true, class: 'width: 15%', headerClass: 'w-full min-w-36 text-center font-bold' },
+    { field: 'locationName', header: 'Location', sortable: true, class: 'width: 15%', headerClass: 'w-full min-w-36 text-center font-bold' },
+    { field: 'personInCharge', header: 'Person In Charge', sortable: true, class: 'width: 20%; text-align: center;', headerClass: 'w-full min-w-20 text-center font-bold' },
+    { field: 'status', header: 'Status', sortable: true, class: 'width: 20%; text-align: center;', headerClass: 'w-full min-w-36 text-center font-bold' }
+]);
+
+const nestedMenuitems = ref([
+    {
+        label: 'Approve',
+        icon: 'pi pi-check-circle',
+        command: () => {
+
+        }
+    },
+    {
+        label: 'Generate Invoice',
+        icon: 'pi pi-check-circle',
+        command: () => {
+
+        }
+    },
+    {
+        label: 'Print',
+        icon: 'pi pi-print',
+        command: () => {
+
+        }
+    },
+    {
+        label: 'Export',
+        items: [
+            {
+                label: 'Excel',
+                icon: 'pi pi-fw pi-compass'
+            },
+            {
+                label: 'CSV',
+                icon: 'pi pi-fw pi-map-marker'
+            }
+        ]
+    }
+]);
+const statusTheme = (statusId: number) => {
+    return statusService.getStatusTheme(statusId)
+}
 const sortedItems = computed(() => {
     return items.value
 })
 
-const columns = [
-    { field: 'TakingNo', header: 'Taking No', sortable: true, customBody: true },
-    { field: 'TakingDate', header: 'Taking Date', sortable: true },
-    { field: 'WarehouseName', header: 'Warehouse', sortable: true },
-    { field: 'LocationName', header: 'Location', sortable: true },
-    { field: 'PersonInCharge', header: 'Person In Charge', sortable: true },
-    { field: 'Status', header: 'Status', customBody: true }
-];
-
-
-const fetchData = async () => {
-  try {
-    const response = await firstValueFrom(
-      StockTakingService.search(`${currentPage.value}/${pageSize.value}/${sortKey.value}/${sortOrder.value}/${searchString.value}`)
-    );
-    
-    console.log('Search result:', response);
-    items.value = response.Data;
-    totalRecords.value = response.Pagination.TotalRecords;
-    totalPages.value = response.Pagination.TotalPages;
-  } catch (error) {
-    console.error('Error fetching data:', error);
-  }
-};
-
-const approves = async (docIds: number[]) => {
-  try {
-    const response = await firstValueFrom(StockTakingService.approves(docIds));
-    console.log('Approved stock taking:', response);
-  } catch (error) {
-    console.error('Error approving stock taking:', error);
-  }
-};
-
-const exportToCSV = () => {
-    console.log('Export CSV');
+const onPageChange = (event: DataTablePageEvent) => {
+    currentPage.value = event.page + 1;
+    pageSize.value = event.rows;
+    fetchData();
 }
 
-const exportToExcel = () => {
-    console.log('Export Excel');
+const clearSearch = () => {
+    currentPage.value = 1
+    pageSize.value = 10
+    sortKey.value = 'takingDate'
+    sortOrder.value = 'DESC'
+    searchString.value = ''
+    fetchData()
 }
+
+const fetchData = () => {
+    fetchLoading.value = true;
+    const endpoint = `${currentPage.value}/${pageSize.value}/${sortKey.value}/${sortOrder.value}/${searchString.value}`;
+    subscription = stockTakingService.search(endpoint).subscribe({
+        next: (result) => {
+            if (result.isSuccess) {
+                items.value = result.data || [];
+                dataTableStore.setTotalRecords(result.pagination?.totalRecords ?? 0);
+            } else {
+                const statusCode = result.statusCode.toString() || 'Unknown';
+                const errorMessage = result.error?.message || 'An error occurred';
+                toast.add({ severity: 'error', summary: statusCode, detail: errorMessage, life: 2000 });
+            }
+        },
+        error: (error) => {
+            toast.add({ severity: 'error', summary: 'Error fetching data', detail: error, life: 2000 });
+        },
+        complete: () => {
+            fetchLoading.value = false;
+        }
+    });
+}
+
+const filteredMenuItems = computed(() => {
+    return nestedMenuitems.value.filter(item => {
+        if (item.label === 'Approve' || item.label === 'Generate Invoice' || item.label === 'Print') {
+            return selectedItems.value.length > 0;
+        }
+        return true;
+    });
+});
 
 const search = () => {
     currentPage.value = 1
     fetchData()
 }
 
-const handleAction = (data: any, action: string) => {
-    switch (action) {
-        case 'detail':
-            router.push(`/StockTaking/Detail/${data.TakingId}`);
-            break;
-        case 'edit':
-            break;
-        case 'copy':
-            break;
-        case 'print':
-            break;
-        case 'cancel':
-            break;
-    }
+const onRowSelect = (value: any[]) => {
+    selectedItems.value = value;
+    isSelectAll.value = selectedItems.value.length === sortedItems.value.length
 };
-
-const onRowSelect = (data: any) => {
-    const takingId = data.TakingId;
-    if (!selectedTakingIds.value.includes(takingId)) {
-        selectedTakingIds.value.push(takingId);
-    }
-};
-
-const onRowUnselect = (data: any) => {
-    const takingId = data.TakingId;
-    const index = selectedTakingIds.value.indexOf(takingId);
-    if (index > -1) {
-        selectedTakingIds.value.splice(index, 1);
-    }
-};
-
-const sortBy = (key: string) => {
-    console.log('Sort by:', sortKey.value);
-    if (key === sortKey.value) {
-        sortOrder.value = sortOrder.value === 'ASC' ? 'DESC' : 'ASC'
-    } else {
-        sortKey.value = key
-        sortOrder.value = 'DESC'
-    }
-    fetchData()
-}
-
-const goToPage = (page: number) => {
-    if (page >= 1 && page <= totalPages.value) {
-        currentPage.value = page
-        fetchData()
-    }
-}
 
 onMounted(() => {
-    fetchData();
-});
+    fetchData()
+})
+
+onUnmounted(() => {
+    subscription.unsubscribe();
+})
 
 watch(selectedItems, (newSelectedItems) => {
-    selectedTakingIds.value = newSelectedItems.map((item: any) => item.TakingId);
+
 });
 
 </script>
-
-<style scoped>
-.hoverTable {
-    width: 100%;
-    border-collapse: collapse;
-}
-
-.hoverTable tbody td {
-    padding: 7px;
-    /*border: #4e95f4 1px solid;*/
-}
-
-.hoverTable tbody tr {
-    background: white;
-}
-
-.hoverTable tbody tr:hover {
-    background-color: #f6f6f6;
-}
-
-.pagination {
-    display: inline-block;
-    cursor: pointer;
-}
-
-.pagination a {
-    color: black;
-    float: left;
-    padding: 8px 16px;
-    text-decoration: none;
-}
-
-.pagination a.active {
-    background-color: #4CAF50;
-    color: white;
-    border-radius: 5px;
-}
-
-.pagination a:hover:not(.active) {
-    background-color: #ddd;
-    border-radius: 5px;
-}
-</style>
